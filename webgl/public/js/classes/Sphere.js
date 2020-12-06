bac9de05ef391a8d5a2d7532add03039f7b66f89
yoong2world <56786806+yoong2world@users.noreply.github.com>
2020-12-07 01:22:33 +0900

    update

--- webgl/public/js/classes/Sphere.js
+++ webgl/public/js/classes/Sphere.js
@@ -159,7 +159,156 @@ export default class Sphere{
     //   geometry.vertices.push(
     //     new THREE.Vector2( 0.0625,  1 ),
     //   );
-    
+
+
+    //create sphere mesh
+    t.sphereGeometry = new THREE.SphereGeometry(
+        1, // radius -- default 1
+        25, // widthSegments -- default 8
+        13, // heightSegments -- default 6
+        //8, // widthSegments -- default 8
+        //6, // heightSegments -- default 6
+        0, // phiStart -- default 0
+        Math.PI * 2, // phiLength -- default Math.PI * 2
+        0, // thetaStart -- default 0
+        Math.PI // thetaLength -- default Math.PI
+    )
+
+
+
+
+    // Geometry
+    t.geometry = new THREE.SphereGeometry(1, 8, 6);
+
+    t.loader = new THREE.TextureLoader;
+    t.loader.crossOrigin = '';
+    t.loader.setPath( '/img/work/' );
+
+    t.sphereMesh = new THREE.Mesh(
+        t.sphereGeometry,
+        new THREE.MeshPhongMaterial( {
+          color: 0x156289,
+          emissive: 0x072534,
+          side: THREE.DoubleSide,
+          flatShading: true,
+          // map: this.loader.load('/img/work/doosan.png'),
+          // bumpMap: this.loader.load('/img/work/doosan.png'),
+        } )
+    );
+
+    let materials = [];
+
+    let arrImages = [
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+      'armypedia.png',
+      'doosanemerging.png',
+      'doosaninfra.png',
+      'doosan.png',
+      'hankook.png',
+      'jung.png',
+      'kcs.png',
+      'lifeplus.png',
+    ];
+
+    //console.log(t.sphereMesh,'spher');
+    // var geometry = new THREE.Geometry();
+
+    //   geometry.vertices.push(
+    //     new THREE.Vector2( 0.0625,  1 ),
+    //   );
+
     // var face;
     // face = new THREE.Face3 (0,1,5); face.materialIndex = 0;
     // geometry.faces.push (face);
@@ -169,8 +318,8 @@ export default class Sphere{
     // geometry.faces.push (face);
     // face = new THREE.Face3 (2,3,4); face.materialIndex = 1;
     // geometry.faces.push (face);
-    
-    
+
+
     // face = new THREE.Face3 (0,3,1); face.materialIndex = 3;
     // geometry.faces.push (face);
     // face = new THREE.Face3 (1,3,2); face.materialIndex = 3;
@@ -180,10 +329,10 @@ export default class Sphere{
     // face = new THREE.Face3 (0,5,3); face.materialIndex = 2;
     // geometry.faces.push (face);
 
-    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(0,0), new THREE.Vector2(1,0), new THREE.Vector2(0,1)] );				 
-    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(1,0), new THREE.Vector2(1,1), new THREE.Vector2(0,1)] );				 
-    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(1,0), new THREE.Vector2(1,1), new THREE.Vector2(0,1)] );				 
-    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(0,0), new THREE.Vector2(1,0), new THREE.Vector2(0,1)] );				 
+    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(0,0), new THREE.Vector2(1,0), new THREE.Vector2(0,1)] );
+    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(1,0), new THREE.Vector2(1,1), new THREE.Vector2(0,1)] );
+    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(1,0), new THREE.Vector2(1,1), new THREE.Vector2(0,1)] );
+    // geometry.faceVertexUvs[0].push( [new THREE.Vector2(0,0), new THREE.Vector2(1,0), new THREE.Vector2(0,1)] );
 
     // geometry.computeBoundingSphere();
     // geometry.computeFaceNormals();
@@ -194,13 +343,13 @@ export default class Sphere{
     // materialArray.push(new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'images/confucius.jpg' ) }));
     // materialArray.push(new THREE.MeshBasicMaterial( {color: 0x009999}))
     // materialArray.push(new THREE.MeshBasicMaterial( {color: 0x888888}))
-    
-    t.sphereGeometry.faces.forEach(function(face, i) {
-      let texture = t.loader.load( arrImages[i] )
-      materials[i] = new THREE.MeshBasicMaterial({ map:texture })
-    })
 
-    t.sphereMesh = new THREE.Mesh(t.geometry, materials)
+    //t.sphereGeometry.faces.forEach(function(face, i) {
+    //  let texture = t.loader.load( arrImages[i] )
+    //  materials[i] = new THREE.MeshBasicMaterial({ map:texture })
+    //})
+
+    //t.sphereMesh = new THREE.Mesh(t.geometry, materials)
 
     t.scene.add( t.sphereMesh )
   }
